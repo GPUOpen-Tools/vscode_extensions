ff2f90763189829ad331be35f42b6269347de838
Baumeister, Dominik <dominik.baumeister@amd.com>
2019-10-04 17:36:40 +0200

    Add gfx1012.

--- radeon-gpu-analyzer/src/rgaCommand.ts
+++ radeon-gpu-analyzer/src/rgaCommand.ts
@@ -107,7 +107,7 @@ export abstract class RgaCommand
     private initializeTargetAsic() : Thenable<any>
     {
         // TODO query from rga
-        var picks =  ['gfx1010', 'gfx906', 'gfx902', 'gfx900', 'Ellesmere', 'Fiji', 
+        var picks =  ['gfx1010', 'gfx1012', 'gfx906', 'gfx902', 'gfx900', 'Ellesmere', 'Fiji', 
         'Baffin', 'Bonaire', '"Bristol Ridge"', 'Capeverde', 
         'Carrizo', 'Hainan', 'Hawaii', 
         'Iceland', 'Kalindi', 'Mullins', 'Oland', 
